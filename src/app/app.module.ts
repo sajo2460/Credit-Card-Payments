d1f1ddff2d1a918b86f005517c664091080e9c2a
Joseph Safari <sajo2460@hotmail.com>
2020-05-06 17:35:05 +0100

    Initial Commit - Create App, input, and card-form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
